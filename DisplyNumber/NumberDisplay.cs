ae8022e1297c94e9c13f0bfeb6a26c3fa9de6145
Vindya Gardiyawasam <vindyadgardiyawasam@outlook.com>
2025-09-28 15:35:25 +0530

    Add project files.

---
+++ DisplyNumber/NumberDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplyNumber
+{
+    public class NumberDisplay
+    {
+        
+      public void DisplayNumbers()
+        {
+            Console.WriteLine("Pring Numbers 1 to 10, Starting!");
+            for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine("Number is:"+i);
+            }
+            Console.WriteLine("Printing Done!");
+        }
+    }
+}
---
+++ DisplyNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DisplyNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            NumberDisplay numberDisplay = new NumberDisplay();  
+            numberDisplay.DisplayNumbers();
+            Console.ReadLine();
+        }
+    }
+}
